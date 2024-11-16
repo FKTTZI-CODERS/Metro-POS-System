dd473282168a71f4106279dcb0df21383188a050
Tayyaba Tanveer <125405432+tayyabatanveer@users.noreply.github.com>
2024-11-17 01:19:00 +0500

    made branch manager controller to handle adding cashier+DEO

---
+++ BranchManagerController.java
@@ -0,0 +1,38 @@
+package metropos.controller;
+
+import metropos.model.BranchManager;
+import javax.swing.JOptionPane;
+import java.sql.SQLException;
+
+public class BranchManagerController {
+    
+    private BranchManager branchManager;
+
+    public BranchManagerController() {
+        branchManager = new BranchManager();
+    }
+
+    public void addCashier(String employeeNo, String name, String email, String salary, String branchCode) throws SQLException {
+        if (employeeNo.isEmpty() || name.isEmpty() || email.isEmpty() || salary.isEmpty() || branchCode.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Please fill in all blank fields");
+            return;
+        }
+        if (branchManager.addCashier(employeeNo, name, email,salary, branchCode)) {
+            JOptionPane.showMessageDialog(null, "Cashier added successfully!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Failed to add cashier!");
+        }
+    }
+
+    public void addDataEntryOperator(String employeeNo, String name, String email, String salary, String branchCode) throws SQLException {
+        if (employeeNo.isEmpty() || name.isEmpty() || email.isEmpty()  || salary.isEmpty() || branchCode.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Please fill in all blank fields");
+            return;
+        }
+        if (branchManager.addDataEntryOperator(employeeNo, name, email,  salary, branchCode)) {
+            JOptionPane.showMessageDialog(null, "Data Entry Operator added successfully!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Failed to add Data Entry Operator!");
+        }
+    }
+}
