41753b25788d6f4d83e3c9cc88f79fdf8b5eaa45
Fatima Khan <163330776+fatimaKhan003@users.noreply.github.com>
2024-11-17 03:35:39 +0500

    updated to add tables and changepassword logic

--- User.java
+++ User.java
@@ -49,7 +49,7 @@ public void createTable() throws SQLException
     String sql2 = "CREATE TABLE IF NOT EXISTS Branch(" +
                   "Branch_Code VARCHAR(10) PRIMARY KEY, " +
                   "name VARCHAR(55) NOT NULL UNIQUE, " +
-                  "city VARCHAR(55) NOT NULL UNIQUE, " +
+                  "city VARCHAR(55) NOT NULL, " +
                   "address VARCHAR(55) NOT NULL UNIQUE, " +
                   "phone VARCHAR(11), " +
                   "active BOOLEAN DEFAULT TRUE, " +
@@ -61,45 +61,84 @@ public void createTable() throws SQLException
     // Branch Manager table
     String sql3 = "CREATE TABLE IF NOT EXISTS BranchManager(" +
                   "id INT AUTO_INCREMENT PRIMARY KEY, " +
-                  "manager_Code VARCHAR(10) UNIQUE, " +
                   "name VARCHAR(55), " +
                   "email VARCHAR(255) UNIQUE, " +
                   "password VARCHAR(255) DEFAULT 'Password_123', " +
                   "Branch_Code VARCHAR(10), " +
                   "salary DECIMAL(10,2), " +
-                  "firstLogin BOOLEAN DEFAULT TRUE, "+
                   "FOREIGN KEY (Branch_Code) REFERENCES Branch(Branch_Code) ON DELETE SET NULL, " +
                   "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
     s = conn.createStatement();
     s.executeUpdate(sql3);
     
     // Cashier table
-String sql4 = "CREATE TABLE IF NOT EXISTS Cashier(" +
-              "employeeNo INT AUTO_INCREMENT PRIMARY KEY, " +
-              "Cashier_Code VARCHAR(10) UNIQUE, " +
-              "name VARCHAR(55) NOT NULL, " +
-              "email VARCHAR(55) UNIQUE, " +
-              "password VARCHAR(255) DEFAULT 'Password_123', " +
-              "Branch_Code VARCHAR(10), " +
-              "salary DECIMAL(10,2), " +
-              "FOREIGN KEY (Branch_Code) REFERENCES Branch(Branch_Code) ON DELETE SET NULL, " +
-              "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
-s.executeUpdate(sql4);
-
-// DataEntryOperator table
-String sql5 = "CREATE TABLE IF NOT EXISTS DataEntryOperator(" +
-              "employeeNo INT AUTO_INCREMENT PRIMARY KEY, " +
-              "DEO_Code VARCHAR(10) UNIQUE, " +
-              "name VARCHAR(55) NOT NULL, " +
-              "email VARCHAR(55) UNIQUE, " +
-              "password VARCHAR(255) DEFAULT 'Password_123', " +
-              "Branch_Code VARCHAR(10), " +
-              "salary DECIMAL(10,2), " +
-              "FOREIGN KEY (Branch_Code) REFERENCES Branch(Branch_Code) ON DELETE SET NULL, " +
-              "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
-s.executeUpdate(sql5);
-
-
+    String sql4 = "CREATE TABLE IF NOT EXISTS Cashier(" +
+                  "Cashier_id INT AUTO_INCREMENT PRIMARY KEY, " +
+                  "name VARCHAR(55) NOT NULL UNIQUE, " +
+                  "email VARCHAR(55) UNIQUE, " +
+                  "password VARCHAR(255) DEFAULT 'Password_123', " +
+                  "Branch_Code VARCHAR(10), " +
+                  "salary DECIMAL(10,2), " +
+                  "FOREIGN KEY (Branch_Code) REFERENCES Branch(Branch_Code) ON DELETE SET NULL, " +
+                  "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
+    s = conn.createStatement();
+    s.executeUpdate(sql4);
+    
+    // DataEntryOperator table
+    String sql5 = "CREATE TABLE IF NOT EXISTS DataEntryOperator(" +
+                  "DEO_Code VARCHAR(10) PRIMARY KEY, " +
+                  "name VARCHAR(55) NOT NULL UNIQUE, " +
+                  "email VARCHAR(55) UNIQUE, " +
+                  "password VARCHAR(255) DEFAULT 'Password_123', " +
+                  "Branch_Code VARCHAR(10), " +
+                  "salary DECIMAL(10,2), " +
+                  "FOREIGN KEY (Branch_Code) REFERENCES Branch(Branch_Code) ON DELETE SET NULL, " +
+                  "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
+    s = conn.createStatement();
+    s.executeUpdate(sql5);
+    
+     //vendor table
+     String sql9="CREATE TABLE IF NOT EXISTS Vendor ("+
+                "Vendor_id INT AUTO_INCREMENT PRIMARY KEY,"
+            + " Vendor_Name VARCHAR(255) NOT NULL,"
+            +"Vendor_Contact VARCHAR(255) NOT NULL,"+
+              "Vendor_Address VARCHAR(255) NOT NULL)";
+    s=conn.createStatement();
+    s.executeUpdate(sql9);
+    
+     //Product table
+      String sql8="CREATE TABLE IF NOT EXISTS Product ("+
+                "Product_id INT AUTO_INCREMENT PRIMARY KEY,"
+            + " Product_Name VARCHAR(255) NOT NULL,"
+            +"Category VARCHAR(255) NOT NULL,"+ "Original_Price DECIMAL(10,2) NOT NULL,"+
+            "Sale_Price DECIMAL(10,2) NOT NULL," +"Price_per_Unit DECIMAL(10,2) NOT NULL," +
+              "Price_per_Carton DECIMAL(10,2) NOT NULL," + "Stock_Quantity INT DEFAULT 0," + "Vendor_id INT,"+
+            "FOREIGN KEY (Vendor_id) REFERENCES Vendor(Vendor_id))";
+    s=conn.createStatement();
+    s.executeUpdate(sql8);
+    
+    //sales table
+    String sql6="CREATE TABLE IF NOT EXISTS Sales ("+
+                "sales_id INT AUTO_INCREMENT PRIMARY KEY, "+
+            "Product_id int NOT NULL, "+
+            "Branch_code VARCHAR(10) NOT NULL, " +
+            "Quantity_Sold INT NOT NULL CHECK(Quantity_Sold >=0),"+
+            "Total_Price DECIMAL (10,2) NOT NULL CHECK(Total_Price >=0), " +
+            "Sale_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "+
+            "FOREIGN KEY (Branch_Code) REFERENCES Branch(Branch_Code) ON DELETE CASCADE,"+"FOREIGN KEY (Product_id) REFERENCES Product(Product_id) ON DELETE CASCADE)";
+    s=conn.createStatement();
+    s.executeUpdate(sql6);
+    
+    //stock table
+    String sql7="CREATE TABLE IF NOT EXISTS Stock ("+
+                "stock_id INT AUTO_INCREMENT PRIMARY KEY,"
+            + " Branch_code VARCHAR(10) NOT NULL,"+"Product_id INT NOT NULL, "
+            +"Category VARCHAR(255) NOT NULL,"+
+            "Quantity_Remaining INT NOT NULL," +
+            " Last_Updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "+
+            "FOREIGN KEY (Branch_Code) REFERENCES Branch(Branch_Code) ON DELETE CASCADE," +"FOREIGN KEY (Product_id) REFERENCES Product(Product_id) ON DELETE CASCADE)";
+    s=conn.createStatement();
+    s.executeUpdate(sql7);
 }
 
         
@@ -115,7 +154,7 @@ public boolean login(String email , String pass, String type) throws SQLExceptio
         ResultSet res = ps.executeQuery();
         return res.next();  
     }
-    else if (type.equals("BranchManager"))
+    else if (type.equals("Branch Manager"))
     {
         String query= "Select * from BranchManager where email = ? AND password = ?";
          PreparedStatement ps= conn.prepareStatement(query);
@@ -144,18 +183,36 @@ else
     }
     
 }
-public boolean checkFirstLogin(String email) throws SQLException {
-    String query = "SELECT firstLogin FROM BranchManager WHERE email = ?";
-    PreparedStatement ps = conn.prepareStatement(query);
-    ps.setString(1, email);
-    ResultSet res = ps.executeQuery();
-    
-    if (res.next()) {
-        return res.getBoolean("firstLogin");
+public boolean isFirstLogin(String email,String  type) throws SQLException
+{String query="";
+    if(type.equalsIgnoreCase("Cashier"))
+{ query= "select password from cashier where email=?";}
+    else if(type.equalsIgnoreCase("Branch Manager"))
+    {query= "select password from BranchManager where email=?";
+        
+    }
+    PreparedStatement ps= conn.prepareStatement(query);
+    ps.setString(1,email);
+    ResultSet res= ps.executeQuery();
+    if(res.next())
+    {
+        String pass= res.getString(("password"));
+        return "Password_123".equals(pass);
     }
     return false;
 }
-
-
+public void changePass(String newpass,String email,String type) throws SQLException
+{String query="";
+    if(type.equalsIgnoreCase("Cashier"))
+    {query="update cashier set password= ? where email=?";}
+    else if(type.equalsIgnoreCase("Branch Manager"))
+    {
+        query="update BranchManager set password= ? where email=?";
+    }
+    PreparedStatement ps = conn.prepareStatement(query);
+    ps.setString(1,newpass);
+    ps.setString(2,email);
+    ps.executeUpdate();
+}
 
 }
